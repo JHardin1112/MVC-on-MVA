a10a44373029a2a108a797ef07c306fa8b0df3e2
Jeremy Hardin <jeremyhardin@gmail.com>
2018-04-03 14:27:27 -0400

    Add project files.

---
+++ MVC on MVA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_on_MVA.MvcApplication" Language="C#" %>
---
+++ MVC on MVA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_on_MVA.Startup))]
+namespace MVC_on_MVA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
